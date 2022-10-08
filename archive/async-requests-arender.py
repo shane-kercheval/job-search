76b5d3321ecf74e4ffac419a4a839cbff91e7993
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2022-10-08 14:54:39 -0700

    Update async-requests-arender.py

--- archive/async-requests-arender.py
+++ archive/async-requests-arender.py
@@ -3,11 +3,6 @@ from bs4 import BeautifulSoup
 
 asession = AsyncHTMLSession()
 
-urls = [
-    'https://careers.chime.com/job/6131676002/Senior-Data-Analyst-Corporate-Strategy',
-    'https://careers.chime.com/job/6207420002/Lead-Data-Scientist-Marketing-Experimentation',
-    'https://careers.chime.com/job/6289551002/Staff-Data-Analyst-Lifecycle-Marketing',
-]
 
 
 def extract_job_description(html: str) -> str:
@@ -17,12 +12,32 @@ def extract_job_description(html: str) -> str:
     return job_description[0].text[0:100]
 
 
-async def extract_javascript(url):
+async def arender(url: str) -> str:
+    """
+    returns tuple including url and html (url is returned because asession.run does not seem to
+    ensure the same order is returned)
+    """
     r = await asession.get(url=url)
     await r.html.arender()
-    return extract_job_description(html=r.html.html)
+    return url, r.html.html
 
 
-# warning does not return in same order
-result = asession.run(*[lambda: extract_javascript(url) for url in urls])
-print(result)
+def extract_descriptions(urls: list[str]) -> list[str]:
+    assert len(urls) == len(set(urls))  # ensure unique urls
+    # url=url explained in these links
+    # https://stackoverflow.com/questions/67656204/how-can-i-build-a-list-of-async-tasks-with-argument-for-asynchtmlsession-run
+    # https://docs.python.org/3.4/faq/programming.html#why-do-lambdas-defined-in-a-loop-with-different-values-all-return-the-same-result
+    # warning does not return in same order
+    results = asession.run(*[lambda url=url: arender(url) for url in urls])
+    assert set(urls) == set(x[0] for x in results)
+    # ensure we return the job descriptions in the same order of the urls passed in
+    results = dict(results)  # keys are urls and values are descriptions
+    return [extract_job_description(html=results[x]) for x in urls]
+
+
+urls = [
+    'https://careers.chime.com/job/6131676002/Senior-Data-Analyst-Corporate-Strategy',
+    'https://careers.chime.com/job/6207420002/Lead-Data-Scientist-Marketing-Experimentation',
+    'https://careers.chime.com/job/6289551002/Staff-Data-Analyst-Lifecycle-Marketing',
+]
+print(extract_descriptions(urls))
