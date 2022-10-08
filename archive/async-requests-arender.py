9f68b6271a545375efa9f69d657ef717ac5dbf92
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2022-10-08 14:27:44 -0700

    Update async-requests-arender.py

--- archive/async-requests-arender.py
+++ archive/async-requests-arender.py
@@ -3,6 +3,12 @@ from bs4 import BeautifulSoup
 
 asession = AsyncHTMLSession()
 
+urls = [
+    'https://careers.chime.com/job/6131676002/Senior-Data-Analyst-Corporate-Strategy',
+    'https://careers.chime.com/job/6207420002/Lead-Data-Scientist-Marketing-Experimentation',
+    'https://careers.chime.com/job/6289551002/Staff-Data-Analyst-Lifecycle-Marketing',
+]
+
 
 def extract_job_description(html: str) -> str:
     soup = BeautifulSoup(html, 'html.parser')
@@ -11,22 +17,12 @@ def extract_job_description(html: str) -> str:
     return job_description[0].text[0:100]
 
 
-async def get_pythonorg():
-    r = await asession.get('https://careers.chime.com/job/6131676002/Senior-Data-Analyst-Corporate-Strategy')
+async def extract_javascript(url):
+    r = await asession.get(url=url)
     await r.html.arender()
     return extract_job_description(html=r.html.html)
 
 
-async def get_reddit():
-    r = await asession.get('https://careers.chime.com/job/6207420002/Lead-Data-Scientist-Marketing-Experimentation')
-    await r.html.arender()
-    return extract_job_description(html=r.html.html)
-
-
-async def get_google():
-    r = await asession.get('https://careers.chime.com/job/6289551002/Staff-Data-Analyst-Lifecycle-Marketing')
-    await r.html.arender()
-    return extract_job_description(html=r.html.html)
-
-result = asession.run(get_pythonorg, get_reddit, get_google)
+# warning does not return in same order
+result = asession.run(*[lambda: extract_javascript(url) for url in urls])
 print(result)
