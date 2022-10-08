4b95f18385de7703a7613006eb47a527928424cb
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2022-10-08 15:32:22 -0700

    Update test_jobs_scraper.py

--- tests/test_jobs_scraper.py
+++ tests/test_jobs_scraper.py
@@ -7,7 +7,7 @@ import yaml
 from source.jobs_scraper import JobInfo, JobScraperBase
 from source.scrapers.vercel import VercelJobScraper
 from source.scrapers.anaconda import AnacondaJobScraper
-from source.scrapers.chime_analytics import ChimeAnalyticsJobScraper
+# from source.scrapers.chime_analytics import ChimeAnalyticsJobScraper
 
 
 class VercelMockJobScraper(JobScraperBase):
@@ -278,9 +278,9 @@ def test_anaconda():
         yaml.dump([job_to_dict(x) for x in jobs], outfile)
 
 
-def test_chime_analytics():
-    scraper = ChimeAnalyticsJobScraper()
-    jobs = scraper.scrape()
-    assert len(jobs) > 0
-    with open('tests/test_files/extracted_jobs/chime_analytics_jobs.yml', 'w') as outfile:
-        yaml.dump([job_to_dict(x) for x in jobs], outfile)
+# def test_chime_analytics():
+#     scraper = ChimeAnalyticsJobScraper()
+#     jobs = scraper.scrape()
+#     assert len(jobs) > 0
+#     with open('tests/test_files/extracted_jobs/chime_analytics_jobs.yml', 'w') as outfile:
+#         yaml.dump([job_to_dict(x) for x in jobs], outfile)
