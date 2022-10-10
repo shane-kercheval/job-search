6391b78d5f48556d96b1041ff59e3b28588c27a1
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2022-10-09 21:25:53 -0700

    Update job_info_database.py

--- source/service/job_info_database.py
+++ source/service/job_info_database.py
@@ -1,21 +1,25 @@
+from datetime import datetime
+import pandas as pd
 from source.entities.job_info import JobInfo, from_dataframe, to_dataframe
 from helpsk.database_base import Database
 from helpsk.pandas import relocate
 
 
-def save_job_infos(database: Database, jobs: list[JobInfo]):
+def save_job_infos(database: Database, jobs: list[JobInfo], snapshot: datetime):
     df = to_dataframe(jobs=jobs)
-    from datetime import datetime
-
-    df['snapshot'] = datetime.now().strftime('%Y-%m-%d %h:%M:%s')
-    df = relocate(df, column='snapshot', before='title')
-
+    df['snapshot'] = snapshot  # datetime.now().strftime('%Y-%m-%d %h:%M:%s')
+    df = relocate(df, column='snapshot', before='company')
     with database:
         database.insert_records(dataframe=df, table='JOBS')
 
 
-def load_job_infos(database: Database):
+def load_job_infos(database: Database) -> pd.DataFrame:
     with database:
         df = database.query('SELECT * FROM JOBS')
+    return from_dataframe(df=df)
 
+
+def load_job_infos_latest(database: Database) -> pd.DataFrame:
+    with database:
+        df = database.query('SELECT * FROM JOBS WHERE snapshot = (SELECT MAX(snapshot) FROM JOBS)')
     return from_dataframe(df=df)
